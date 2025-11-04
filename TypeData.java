a748329eb7c6cd64025f9e94e55eaa366ef73eb4
herlanalansuherman31 <herlanalansuherman06@gmail.com>
2025-11-04 21:37:09 +0700

    menambahkan tipe data dari bahasa pemograman java

---
+++ TypeData.java
@@ -0,0 +1,14 @@
+public class TypeData {
+    public static void main(String[]args) {
+        byte nilai_byte = 100;
+		short nilai_short = 30000;
+		int nilai_int = 12000000;
+		long nilai_long = 50000000000L;
+		
+		System.out.println("nilai tipe data byte =" + nilai_byte);
+		System.out.println("nilai tipe data short =" + nilai_short);
+		System.out.println("nilai tipe data int =" + nilai_int);
+		System.out.println("nilai tipe data long =" + nilai_long);
+        
+    }
+}
